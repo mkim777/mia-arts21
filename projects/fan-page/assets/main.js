dfcf0558d793ca197357321ad28d79801eef9040
mkim777 <78634416+mkim777@users.noreply.github.com>
2021-03-20 19:27:22 -0400

    update 3/20

--- projects/fan-page/assets/main.js
+++ projects/fan-page/assets/main.js
@@ -1,5 +1,24 @@
-/* var headerHover = document.getElementByClassName("header");
+let headerCounter = 0;
+let subheaderCounter = 0;
+let poemCounter = 0;
 
-headerHover.addEventListener("mouseover", function() {
-    headerHover.setAttribute("style", "opacity: 1;");
-}); */
+if (headerCounter == 0) {
+	$(".header").mouseover(function() {
+		$(".header").fadeTo(800, 1);
+		headerCounter++;
+	});
+}
+
+if (subheaderCounter == 0) {
+  $(".subheader").mouseover(function() {
+    $(".subheader").fadeTo(800, 1);
+    subheaderCounter++;
+  });
+}
+
+if (poemCounter == 0) {
+  $(".poem").mouseover(function() {
+    $(".poem").fadeTo(800, 1);
+    poemCounter++;
+  });
+}
