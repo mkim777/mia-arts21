01e563aa96866de75ca542651b782bc4ed9c11ab
mkim777 <78634416+mkim777@users.noreply.github.com>
2021-03-20 20:10:39 -0400

    update 3/20

--- projects/fan-page/assets/main.js
+++ projects/fan-page/assets/main.js
@@ -2,23 +2,37 @@ let headerCounter = 0;
 let subheaderCounter = 0;
 let poemCounter = 0;
 
-if (headerCounter == 0) {
-	$(".header").mouseover(function() {
-		$(".header").fadeTo(800, 1);
-		headerCounter++;
-	});
-}
+if (window.innerWidth > 768) {
+  if (headerCounter == 0) {
+    $(".header").mouseover(function() {
+      $(".header").fadeTo(800, 1);
+      headerCounter++;
+    });
+    $(".gunpowder-header").mouseover(function() {
+      $(".gunpowder-header").fadeTo(800, 1);
+      headerCounter++;
+    });
+  }
 
-if (subheaderCounter == 0) {
-  $(".subheader").mouseover(function() {
-    $(".subheader").fadeTo(800, 1);
-    subheaderCounter++;
-  });
-}
+  if (subheaderCounter == 0) {
+    $(".subheader").mouseover(function() {
+      $(".subheader").fadeTo(800, 1);
+      subheaderCounter++;
+    });
+    $(".gunpowder-subheader").mouseover(function() {
+      $(".gunpowder-subheader").fadeTo(800, 1);
+      subheaderCounter++;
+    });
+  }
 
-if (poemCounter == 0) {
-  $(".poem").mouseover(function() {
-    $(".poem").fadeTo(800, 1);
-    poemCounter++;
-  });
+  if (poemCounter == 0) {
+    $(".poem").mouseover(function() {
+      $(".poem").fadeTo(800, 1);
+      poemCounter++;
+    });
+    $(".gunpowder-poem").mouseover(function() {
+      $(".gunpowder-poem").fadeTo(800, 1);
+      poemCounter++;
+    });
+  }
 }
