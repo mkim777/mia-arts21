f38c5ebeb489b9fe9fb48f3dc04e77ae10f420ba
mkim777 <78634416+mkim777@users.noreply.github.com>
2021-04-27 21:16:20 -0400

    update 4/27

---
+++ assets/main.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+	$(".news-room-link").mouseenter(function() {
+		$(".news-room").fadeTo(300, 1);
+		$("body").css("background-color", "white");
+	});
+
+	$(".news-room-link").mouseleave(function () {
+		$(".news-room").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".fan-page-link").mouseenter(function() {
+		$(".fan-page").fadeTo(300, 1);
+		$("body").css("background-color", "#EFEFEF");
+	});
+
+	$(".fan-page-link").mouseleave(function () {
+		$(".fan-page").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})	
+
+	$(".helpful-app-link").mouseenter(function() {
+		$(".helpful-app").fadeTo(300, 1);
+		$("body").css("background-color", "#FFF9E7");
+	});
+
+	$(".helpful-app-link").mouseleave(function () {
+		$(".helpful-app").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".clock-link").mouseenter(function() {
+		$(".clock").fadeTo(300, 1);
+		$("body").css("background-color", "#F6BC42");
+	});
+
+	$(".clock-link").mouseleave(function () {
+		$(".clock").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".css-animation-link").mouseenter(function() {
+		$(".css-animation").fadeTo(300, 1);
+		$("body").css("background-color", "#FEF3C7");
+	});
+
+	$(".css-animation-link").mouseleave(function () {
+		$(".css-animation").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".flexbox-poster-link").mouseenter(function() {
+		$(".flexbox-poster").fadeTo(300, 1);
+		$("body").css("background-color", "#F6EDEB");
+	});
+
+	$(".flexbox-poster-link").mouseleave(function () {
+		$(".flexbox-poster").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".hover-link").mouseenter(function() {
+		$(".hover").fadeTo(300, 1);
+		$("body").css("background-color", "#8D93E0");
+	});
+
+	$(".hover-link").mouseleave(function () {
+		$(".hover").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".interview-link").mouseenter(function() {
+		$(".interview").fadeTo(300, 1);
+		$("body").css("background-color", "white");
+	});
+
+	$(".interview-link").mouseleave(function () {
+		$(".interview").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".jquery-link").mouseenter(function() {
+		$(".jquery").fadeTo(300, 1);
+		$("body").css("background-color", "#DCB64F");
+	});
+
+	$(".jquery-link").mouseleave(function () {
+		$(".jquery").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+
+	$(".wikipedia-link").mouseenter(function() {
+		$(".wikipedia").fadeTo(300, 1);
+		$("body").css("background-color", "#D1C8B8");
+	});
+
+	$(".wikipedia-link").mouseleave(function () {
+		$(".wikipedia").fadeTo(300, 0);
+		$("body").css("background-color", "white");
+	})
+});
